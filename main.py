eb5a6c2c86ba2d12e849be72f15d402a6a97affe
ʀᴀᴠᴇɴ <thepettypow@gmail.com>
2024-06-10 18:56:50 +0330

    added base code

---
+++ main.py
@@ -0,0 +1,62 @@
+"""Get song titles and artists from Spotify playlist"""
+
+import csv
+import os
+import re
+
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# load credentials from .env file
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID", "")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
+OUTPUT_FILE_NAME = "tracks.csv"
+
+# change for your target playlist
+PLAYLIST_LINK = "https://open.spotify.com/playlist/6nGX6s6mecH0mJAyUUr2Ws?si=a39da8a7ccf3475e"
+
+
+def stc():
+    # authenticate
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET
+    )
+
+    # create spotify session object
+    session = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # get uri from https link
+    if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", PLAYLIST_LINK):
+        playlist_uri = match.groups()[0]
+    else:
+        raise ValueError("Expected format: https://open.spotify.com/playlist/uri")
+
+    # get list of tracks in a given playlist (note: max playlist length 100)
+    tracks = session.playlist_tracks(playlist_uri)["items"]
+
+    # create csv file
+    with open(OUTPUT_FILE_NAME, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        # write header column names
+        writer.writerow(["track", "artist"])
+
+        # extract name and artist
+        for track in tracks:
+            name = track["track"]["name"]
+            artists = ", ".join(
+                [artist["name"] for artist in track["track"]["artists"]]
+            )
+
+            # write to csv
+            writer.writerow([name, artists])
+
+def cty():
+
+
+
+def main():
+    stc()
